35d1715ec4f26df324cb74b9c1d8e332e7b844ba
zaidur <rahmanzaidur@gmail.com>
2022-03-05 13:07:28 -0500

    homework

---
+++ src/Class1/HomeWork2.java
@@ -0,0 +1,39 @@
+package Class1;
+
+public class HomeWork2 {
+    public static void main(String[] args) {
+        byte oneDay = 24;
+        short oneYear = 366;
+       int employees = 500;
+       long population = 329500000;
+       float mortgageRate = 2.99f;
+       double accountBalance= 4321.56;
+       boolean sunRise = false;
+       char initials1 = 'M';
+       char initials2 = 'R';    // My initials are MR
+       String  myName ="Mohammed Zaidur Rahman";
+
+        System.out.println("\n1 Number of hours in a day is = "+oneDay);
+        System.out.println("\n2 Max number of days in a year is = " +oneYear);
+        System.out.println("\n3 Total number of employees in  my  organization is = " +employees);
+        System.out.println("\n4 Population in usa are = " +population);
+        System.out.println("\n5 My home mortgage rate is = " +mortgageRate );
+        System.out.println("\n6 Balance of Jhon's bank account is = " +accountBalance);
+        System.out.println("\n7 Does the sun rise from the west? = "+sunRise );
+        System.out.println("\n8 My initials are = " +initials1 +initials2  );
+        System.out.println("\n9 My name is = "+myName);
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
